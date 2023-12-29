5c3d0c3532d2d7c82e422ee47265fdac52b1f25d
Mark Clearwater <csmacnz@csmac.co.nz>
2023-12-29 23:03:05 +1300

    2023 day 12 part 2 solved added caching memoisation

--- 2023/rust/src/bin/day12/main.rs
+++ 2023/rust/src/bin/day12/main.rs
@@ -1,4 +1,5 @@
-use std::{fs, iter};
+use std::{collections::HashMap, fs};
+type Cache = HashMap<(String, Option<u64>, Vec<u64>), u64>;
 
 #[derive(Clone)]
 struct Spring {
@@ -23,7 +24,27 @@ fn parse_file(path: &str) -> Vec<Spring> {
         .collect()
 }
 
-fn count_arrangements(state: &str, current: Option<u64>, remainder_counts: &[u64]) -> u64 {
+fn count_arrangements_cache(
+    state: &str,
+    current: Option<u64>,
+    remainder_counts: &[u64],
+    cache: &mut Cache,
+) -> u64 {
+    let key = (state.to_owned(), current, remainder_counts.to_vec());
+    if let Some(result) = cache.get(&key) {
+        return *result;
+    }
+    let result = count_arrangements(state, current, remainder_counts, cache);
+    cache.insert(key, result);
+    result
+}
+
+fn count_arrangements(
+    state: &str,
+    current: Option<u64>,
+    remainder_counts: &[u64],
+    cache: &mut Cache,
+) -> u64 {
     if state.is_empty() {
         if !remainder_counts.is_empty() || current.is_some_and(|x| x > 0) {
             0
@@ -33,7 +54,7 @@ fn count_arrangements(state: &str, current: Option<u64>, remainder_counts: &[u64
     } else {
         if state.starts_with('.') {
             if current.is_none() || current == Some(0) {
-                count_arrangements(&state[1..], None, remainder_counts)
+                count_arrangements_cache(&state[1..], None, remainder_counts, cache)
             } else {
                 0
             }
@@ -42,8 +63,8 @@ fn count_arrangements(state: &str, current: Option<u64>, remainder_counts: &[u64
             let mut state_if_operational = state.to_owned();
             state_if_damaged.replace_range(0..1, "#");
             state_if_operational.replace_range(0..1, ".");
-            count_arrangements(&state_if_damaged, current, &remainder_counts)
-                + count_arrangements(&state_if_operational, current, &remainder_counts)
+            count_arrangements_cache(&state_if_damaged, current, &remainder_counts, cache)
+                + count_arrangements_cache(&state_if_operational, current, &remainder_counts, cache)
         } else if state.starts_with('#') {
             if current == Some(0) {
                 0
@@ -51,9 +72,9 @@ fn count_arrangements(state: &str, current: Option<u64>, remainder_counts: &[u64
                 0
             } else if current.is_none() {
                 let (new_current, new_remainder) = remainder_counts.split_first().unwrap();
-                count_arrangements(&state[1..], Some(new_current - 1), &new_remainder)
+                count_arrangements_cache(&state[1..], Some(new_current - 1), &new_remainder, cache)
             } else if let Some(x) = current {
-                count_arrangements(&state[1..], Some(x - 1), remainder_counts)
+                count_arrangements_cache(&state[1..], Some(x - 1), remainder_counts, cache)
             } else {
                 unreachable!();
             }
@@ -65,26 +86,28 @@ fn count_arrangements(state: &str, current: Option<u64>, remainder_counts: &[u64
 
 fn part_1(path: &str) -> u64 {
     let springs = parse_file(path);
+    let mut cache = HashMap::<(String, Option<u64>, Vec<u64>), u64>::new();
     springs
         .iter()
-        .map(|spring| count_arrangements(&spring.state, None, &spring.counts))
+        .map(|spring| count_arrangements_cache(&spring.state, None, &spring.counts, &mut cache))
         .sum()
 }
 
 fn part_2(path: &str) -> u64 {
     let springs = parse_file(path);
 
+    let mut cache = HashMap::<(String, Option<u64>, Vec<u64>), u64>::new();
     springs
         .into_iter()
         .map(|s| -> Spring {
             let counts_len = s.counts.len();
-            let state = vec!(s.state; 5).join("?").to_string();
+            let state = vec![s.state; 5].join("?").to_string();
             Spring {
                 state: state,
                 counts: s.counts.into_iter().cycle().take(counts_len * 5).collect(),
             }
         })
-        .map(|spring| count_arrangements(&spring.state, None, &spring.counts))
+        .map(|spring| count_arrangements_cache(&spring.state, None, &spring.counts, &mut cache))
         .sum()
 }
 
@@ -116,4 +139,12 @@ fn can_parse_part2_sample() {
     let answer = part_2("./src/bin/day12/part1_sample.txt");
 
     assert_eq!(answer, 525152);
+}
+
+
+#[test]
+fn can_parse_part2_puzzle() {
+    let answer = part_2("./src/bin/day12/puzzle.txt");
+
+    assert_eq!(answer, 37366887898686);
 }
