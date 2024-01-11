9dbba8ef60b205a3ff0125f8d34ec361923435ee
Mark Clearwater <csmacnz@csmac.co.nz>
2024-01-11 16:19:26 +1300

    2023 day 17 part 2

--- 2023/rust/src/bin/day17/main.rs
+++ 2023/rust/src/bin/day17/main.rs
@@ -38,6 +38,7 @@ struct Pos(usize, usize);
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 enum Direction {
+    Start,
     Left,
     Right,
     Up,
@@ -47,6 +48,7 @@ enum Direction {
 impl Pos {
     fn mv(self: &Self, dir: Direction, width: usize, height: usize) -> Option<Pos> {
         match dir {
+            Direction::Start => panic!(),
             Direction::Up if self.0 <= 0 => None,
             Direction::Left if self.1 <= 0 => None,
             Direction::Right if self.1 >= (width - 1) => None,
@@ -115,8 +117,16 @@ fn part_1(path: &str) -> u64 {
                 continue;
             }
         }
-        dist.insert((state.position, state.incomming_direction, state.count), state.cost);
+        dist.insert(
+            (state.position, state.incomming_direction, state.count),
+            state.cost,
+        );
         let turns = match state.incomming_direction {
+            Direction::Start => [
+                (Direction::Right, 0),
+                (Direction::Right, 0),
+                (Direction::Down, 0),
+            ],
             Direction::Down | Direction::Up => [
                 (state.incomming_direction, state.count),
                 (Direction::Right, 0),
@@ -146,7 +156,82 @@ fn part_1(path: &str) -> u64 {
 
 fn part_2(path: &str) -> u64 {
     let grid = parse_file(path);
-    0
+
+    // dist[node] = current shortest distance from `start` to `node`
+    let mut dist: HashMap<(Pos, Direction, u64), u64> = HashMap::new();
+
+    let mut heap: BinaryHeap<State> = BinaryHeap::new();
+    let end = Pos(grid.height - 1, grid.width - 1);
+    heap.push(State {
+        cost: 0,
+        position: Pos(0, 0),
+        incomming_direction: Direction::Start,
+        count: 0,
+    });
+    while let Some(state) = heap.pop() {
+        if state.position == end {
+            return state.cost;
+        };
+        if let Some(&heat) = dist.get(&(state.position, state.incomming_direction, state.count)) {
+            if heat <= state.cost {
+                continue;
+            }
+        }
+        dist.insert(
+            (state.position, state.incomming_direction, state.count),
+            state.cost,
+        );
+        let turns = match state.incomming_direction {
+            Direction::Start => [
+                (Direction::Start, 0),
+                (Direction::Right, 0),
+                (Direction::Down, 0),
+            ],
+            Direction::Down | Direction::Up => [
+                (state.incomming_direction, state.count),
+                (Direction::Right, 0),
+                (Direction::Left, 0),
+            ],
+            Direction::Right | Direction::Left => [
+                (state.incomming_direction, state.count),
+                (Direction::Down, 0),
+                (Direction::Up, 0),
+            ],
+        };
+        for (next_dir, step) in turns {
+            if next_dir != Direction::Start {
+                if let Some(next_pos) = grid.mv(state.position, next_dir) {
+                    let can_push = if next_dir == state.incomming_direction {
+                        state.count < 10
+                    } else {
+                        if state.incomming_direction == Direction::Start {
+                            true
+                        }
+                        else if state.count < 4 {
+                            false
+                        } else {
+                            match next_dir {
+                                Direction::Start => panic!(),
+                                Direction::Left => state.position.1 >= 4,
+                                Direction::Right => state.position.1 <= grid.width - 1 - 4,
+                                Direction::Up => state.position.0 >= 4,
+                                Direction::Down => state.position.0 <= grid.height - 1 - 4,
+                            }
+                        }
+                    };
+                    if can_push {
+                        heap.push(State {
+                            cost: state.cost + grid.at(next_pos),
+                            position: next_pos,
+                            incomming_direction: next_dir,
+                            count: step + 1,
+                        });
+                    }
+                }
+            }
+        }
+    }
+    unreachable!();
 }
 
 fn main() {
@@ -173,16 +258,18 @@ fn can_parse_part1_puzzle() {
     assert_eq!(answer, 1110);
 }
 
-// #[test]
+#[test]
 fn can_parse_part2_sample() {
     let answer = part_2("./src/bin/day17/sample.txt");
 
-    assert_eq!(answer, 0);
+    assert_eq!(answer, 94);
 }
 
-// #[test]
+#[test]
 fn can_parse_part2_puzzle() {
     let answer = part_2("./src/bin/day17/puzzle.txt");
 
+    assert_ne!(answer, 1304);
+    assert_ne!(answer, 1303);
     assert_eq!(answer, 0);
 }
