f4b0c9a089c4ef219642a6a4366ec8a489ae1bda
Mark Clearwater <csmacnz@csmac.co.nz>
2024-12-01 23:11:44 +1300

    Day 01 parts 1 and 2 complete

---
+++ 2024/csharp/day01/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
+
+Console.WriteLine("Hello, World!");
+
+var lines = File.ReadLines("puzzle.txt");
+var leftList = new List<int>();
+var rightList = new List<int>();
+foreach (var line in lines)
+{
+    var l = line.Split("   ");
+    var a = int.Parse(l[0]);
+    var b = int.Parse(l[1]);
+    leftList.Add(a);
+    rightList.Add(b);
+}
+leftList.Sort();
+rightList.Sort();
+
+var result1 = leftList.Zip(rightList).Select(((int l, int r) x) => Math.Abs(x.l - x.r)).Sum();
+
+Console.WriteLine($"part 1 is: {result1}");
+
+// Part 2
+var memo = new Dictionary<int, int>();
+var result2 = 0;
+foreach (var number in leftList)
+{
+    if (!memo.ContainsKey(number))
+    {
+        memo[number] = number * rightList.Count(b => b == number);
+    }
+    result2 += memo[number];
+}
+
+Console.WriteLine($"part 2 is: {result2}");
