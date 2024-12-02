fcbb742ec0cf04d67c8876a53825ac94250c93ee
Mark Clearwater <csmacnz@csmac.co.nz>
2024-12-03 12:24:36 +1300

    2024 day02 benchmark + spans

--- 2024/csharp/day02/Program.cs
+++ 2024/csharp/day02/Program.cs
@@ -1,74 +1,112 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Runtime.InteropServices;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
 Console.WriteLine("Hello, World!");
 
-var lines = File.ReadAllLines("puzzle.txt");
-var count = 0;
-foreach (var line in lines)
-{
-    var nums = line.Split(' ').Select(n => int.Parse(n)).ToArray();
-    var first = nums[0];
-    var matches = growingCheck(nums, []) || shrinkingCheck(nums, []);
-    if (matches)
-    {
-        count++;
-    }
-}
+BenchmarkRunner.Run<Day02>();
 
-Console.WriteLine($"part 1: {count}");
-
-count = 0;
-foreach (var line in lines)
+[MemoryDiagnoser]
+public class Day02
 {
-    var nums = line.Split(' ').Select(n => int.Parse(n)).ToArray();
-    var first = nums[0];
-    var matches = nums[1] switch
+    [Benchmark]
+    public void Part1()
     {
-        var x when x > first => growingCheck(nums, []),
-        var y when y < first => shrinkingCheck(nums, []),
-        _ => false
-    };
-    if (!matches)
-    {
-        for (var i = 0; i < nums.Length; i++)
+        var lines = File.ReadAllLines("puzzle.txt");
+        var count = 0;
+        foreach (var line in lines)
         {
-            var lhs = nums[0..i];
-            var rhs = nums[(i + 1)..];
-
-            matches = growingCheck(lhs, rhs) || shrinkingCheck(lhs, rhs);
+            var nums = ParseLevels(line);
+            var first = nums[0];
+            var matches = growingCheck(nums, []) || shrinkingCheck(nums, []);
             if (matches)
             {
-                break;
+                count++;
             }
         }
+
+        // Console.WriteLine($"part 1: {count}");
     }
-    if (matches)
+
+    [Benchmark]
+    public void Part2()
     {
-        count++;
+        var lines = File.ReadAllLines("puzzle.txt");
+        var count = 0;
+        foreach (var line in lines)
+        {
+            var nums = ParseLevels(line);
+            var first = nums[0];
+            var matches = growingCheck(nums, []) || shrinkingCheck(nums, []);
+            if (!matches)
+            {
+                for (var i = 0; i < nums.Length; i++)
+                {
+                    var lhs = nums[0..i];
+                    var rhs = nums[(i + 1)..];
+
+                    matches = growingCheck(lhs, rhs) || shrinkingCheck(lhs, rhs);
+                    if (matches)
+                    {
+                        break;
+                    }
+                }
+            }
+            if (matches)
+            {
+                count++;
+            }
+        }
+
+        // Console.WriteLine($"part 2: {count}");
     }
-}
 
-Console.WriteLine($"part 2: {count}");
-
-bool growingCheck(Span<int> nums, Span<int> moreNums)
-{
-    return AllPairs(nums, moreNums, (a, b) => a < b && b - a <= 3);
-}
-
-
-bool shrinkingCheck(Span<int> nums, Span<int> moreNums)
-{
-    return AllPairs(nums, moreNums, (a, b) => a > b && a - b <= 3);
-}
-
-bool AllPairs(Span<int> nums, Span<int> moreNums, Func<int, int, bool> check)
-{
-    var iter = nums.GetEnumerator();
-    int? prev = null;
-    if (iter.MoveNext())
+    private static Span<int> ParseLevels(ReadOnlySpan<char> line)
     {
-        prev = iter.Current;
+        int[] results = new int[line.Length];
+        var count = 0;
+        foreach (var number in line.Split(' '))
+        {
+            results[count] =int.Parse(line[number]);
+            count++;
+        }
+        return results.AsSpan()[..count];
+    }
+
+    private static bool growingCheck(Span<int> nums, Span<int> moreNums)
+    {
+        return AllPairs(nums, moreNums, (a, b) => a < b && b - a <= 3);
+    }
+
+
+    private static bool shrinkingCheck(Span<int> nums, Span<int> moreNums)
+    {
+        return AllPairs(nums, moreNums, (a, b) => a > b && a - b <= 3);
+    }
+
+    private static bool AllPairs(Span<int> nums, Span<int> moreNums, Func<int, int, bool> check)
+    {
+        var iter = nums.GetEnumerator();
+        int? prev = null;
+        if (iter.MoveNext())
+        {
+            prev = iter.Current;
+            while (iter.MoveNext())
+            {
+                var curr = iter.Current;
+                if (!check(prev.Value, curr))
+                {
+                    return false;
+                }
+                prev = curr;
+            }
+        }
+        iter = moreNums.GetEnumerator();
+        if (prev is null)
+        {
+            iter.MoveNext();
+            prev = iter.Current;
+        }
         while (iter.MoveNext())
         {
             var curr = iter.Current;
@@ -78,21 +116,6 @@ bool AllPairs(Span<int> nums, Span<int> moreNums, Func<int, int, bool> check)
             }
             prev = curr;
         }
+        return true;
     }
-    iter = moreNums.GetEnumerator();
-    if (prev is null)
-    {
-        iter.MoveNext();
-        prev = iter.Current;
-    }
-    while (iter.MoveNext())
-    {
-        var curr = iter.Current;
-        if (!check(prev.Value, curr))
-        {
-            return false;
-        }
-        prev = curr;
-    }
-    return true;
 }
