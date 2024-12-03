10aa69a18b320fdbdb623610e22a304c5a11b42b
Mark Clearwater <csmacnz@csmac.co.nz>
2024-12-03 19:57:41 +1300

    2024 day03 tranformed to part2 (part1 not retained)

--- 2024/csharp/day03/Program.cs
+++ 2024/csharp/day03/Program.cs
@@ -7,32 +7,48 @@ Console.WriteLine("Hello, World!");
 var lines = File.ReadAllLines("puzzle.txt");
 
 var result = 0;
+var state = State.Do;
 foreach (var line in lines)
 {
-    var state = State.Open;
     int? a = null, b = null;
     foreach (var c in line)
     {
         (state, a, b) = (state, c) switch
         {
-            (State.Open, 'm') => ((State)State.M, (int?)null, (int?)null),
+            (State.Do, 'd') => ((State)State.D, (int?)null, (int?)null),
+            (State.D, 'o') => (State.O, null, null),
+            (State.O, 'n') => (State.N, null, null),
+            (State.N, '\'') => (State.Apostrophy, null, null),
+            (State.Apostrophy, 't') => (State.T, null, null),
+            (State.T, '(') => (State.DontLeftParen, null, null),
+            (State.DontLeftParen, ')') => (State.Dont, null, null),
+            (State.Do, 'm') => (State.M, null, null),
             (State.M, 'u') => (State.U, null, null),
             (State.U, 'l') => (State.L, null, null),
             (State.L, '(') => (State.LeftParen, null, null),
             (State.LeftParen, var digit) when digit >= '0' && digit <= '9' => (State.Num1, int.Parse(digit.ToString()), null),
             (State.Num1, ',') => (State.Comma, a.Value, null),
-            (State.Num1, var digit) when digit >= '0' && digit <= '9' => (State.Num1, a*10+int.Parse(digit.ToString()), null),
+            (State.Num1, var digit) when digit >= '0' && digit <= '9' => (State.Num1, a * 10 + int.Parse(digit.ToString()), null),
             (State.Comma, var digit) when digit >= '0' && digit <= '9' => (State.Num2, a, int.Parse(digit.ToString())),
             (State.Num2, ')') => (State.RightParen, a, b.Value),
-            (State.Num2, var digit) when digit >= '0' && digit <= '9' => (State.Num2, a, b*10+int.Parse(digit.ToString())),
-            (_, _) => (State.Open, null, null)
+            (State.Num2, var digit) when digit >= '0' && digit <= '9' => (State.Num2, a, b * 10 + int.Parse(digit.ToString())),
+            (State.Dont, 'd') => (State.DoD, null, null),
+            (State.Dont, _) => (State.Dont, null, null),
+            (State.DoD, 'o') => (State.DoO, null, null),
+            (State.DoD, _) => (State.Dont, null, null),
+            (State.DoO, '(') => (State.DoLeftParen, null, null),
+            (State.DoO, _) => (State.Dont, null, null),
+            (State.DoLeftParen, ')') => (State.Do, null, null),
+            (State.DoLeftParen, _) => (State.Dont, null, null),
+            (_, _) => (State.Do, null, null)
         };
-        if(state == State.RightParen){
-            Console.WriteLine($"{a.Value}*{b.Value} = {a.Value*b.Value}");
-            result += a.Value*b.Value;
-            a=null;
-            b=null;
-            state = State.Open;
+        if (state == State.RightParen)
+        {
+            Console.WriteLine($"{a.Value}*{b.Value} = {a.Value * b.Value}");
+            result += a.Value * b.Value;
+            a = null;
+            b = null;
+            state = State.Do;
         }
     }
 }
@@ -40,7 +56,7 @@ Console.WriteLine(result);
 
 public enum State
 {
-    Open,
+    Do,
     M,
     U,
     L,
@@ -49,4 +65,15 @@ public enum State
     Comma,
     Num2,
     RightParen,
+    D,
+    O,
+    N,
+    Apostrophy,
+    T,
+    DontLeftParen,
+    Dont,
+    DoD,
+    DoO,
+    DoLeftParen,
+
 }
