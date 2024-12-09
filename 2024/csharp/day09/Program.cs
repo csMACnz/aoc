cb916a12de0a73e5072365e9999740b37c3df00d
Mark Clearwater <csmacnz@csmac.co.nz>
2024-12-09 23:22:40 +1300

    2024 day09 part2 solved

--- 2024/csharp/day09/Program.cs
+++ 2024/csharp/day09/Program.cs
@@ -2,45 +2,112 @@
 using Dumpify;
 
 Console.WriteLine("Hello, World!");
+Console.WriteLine($"Part1: {Part1()}");
+Console.WriteLine($"Part2: {Part2()}");
 
-var line = File.ReadAllText("puzzle.txt");
-
-var data = new List<short>();
-short valueIndex = 0;
-var onGap = false;
-foreach (var ch in line)
+static long Part1()
 {
-    var value = short.Parse([ch]);
-    foreach (var _ in Enumerable.Range(0, value))
-    {
-        data.Add(onGap ? short.MinValue : valueIndex);
-    }
-    if (!onGap)
-    {
-        valueIndex++;
-    }
-    onGap = !onGap;
-}
-long sum = 0;
-var readIndex = 0;
-while (data.Count != 0)
-{
-    short value = data[0];
-    data.RemoveAt(0);
+    var line = File.ReadAllText("puzzle.txt");
 
-    if (value == short.MinValue)
+    var data = new List<short>();
+    short valueIndex = 0;
+    var onGap = false;
+    foreach (var ch in line)
     {
-        while (value == short.MinValue)
+        var value = short.Parse([ch]);
+        foreach (var _ in Enumerable.Range(0, value))
         {
-            if (data.Count == 0) break;
-            value = data[^1];
-            data.RemoveAt(data.Count - 1);
+            data.Add(onGap ? short.MinValue : valueIndex);
         }
-        if (data.Count == 0) break;
+        if (!onGap)
+        {
+            valueIndex++;
+        }
+        onGap = !onGap;
     }
-    Console.WriteLine($"sum += value({value}) * readIndex({readIndex}) ({value * readIndex})");
-    sum += value * readIndex;
-    readIndex++;
+    long sum = 0;
+    var readIndex = 0;
+    while (data.Count != 0)
+    {
+        short value = data[0];
+        data.RemoveAt(0);
+
+        if (value == short.MinValue)
+        {
+            while (value == short.MinValue)
+            {
+                if (data.Count == 0) break;
+                value = data[^1];
+                data.RemoveAt(data.Count - 1);
+            }
+            if (data.Count == 0) break;
+        }
+        sum += value * readIndex;
+        readIndex++;
+    }
+
+    return sum;
 }
 
-Console.WriteLine($"Part1: {sum}");
+static long Part2()
+{
+    var line = File.ReadAllText("puzzle.txt");
+
+    var data = new List<short>();
+    List<(int offset, int size)> gaps = [];
+    List<(int offset, int size, short value)> values = [];
+    short valueIndex = 0;
+    var onGap = false;
+    foreach (var ch in line)
+    {
+        var value = short.Parse([ch]);
+        if (value > 0)
+        {
+            if (onGap)
+            {
+                gaps.Add((data.Count, value));
+            }
+            else
+            {
+                values.Insert(0, (data.Count, value, valueIndex));
+            }
+        }
+        foreach (var _ in Enumerable.Range(0, value))
+        {
+            data.Add(onGap ? short.MinValue : valueIndex);
+        }
+        if (!onGap)
+        {
+            valueIndex++;
+        }
+        onGap = !onGap;
+    }
+
+    foreach (var value in values)
+    {
+        foreach (var gapIndex in Enumerable.Range(0, gaps.Count))
+        {
+            var gap = gaps[gapIndex];
+            if (gap.offset > value.offset) break;
+            if (gap.size >= value.size)
+            {
+                for (var i = 0; i < value.size; i++)
+                {
+                    data[gap.offset + i] = value.value;
+                    data[value.offset + i] = short.MinValue;
+                }
+                if (gap.size == value.size)
+                {
+                    gaps.RemoveAt(gapIndex);
+                }
+                else
+                {
+                    gaps[gapIndex] = (gap.offset + value.size, gap.size - value.size);
+                }
+                break;
+            }
+        }
+    }
+    long sum = data.Select<short, long>((val, index) => val == short.MinValue ? (long)0 : (long)val * (long)index).Sum();
+    return sum;
+}
