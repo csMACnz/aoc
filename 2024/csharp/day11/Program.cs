61416ad9a650702c4537f93100b3d6364d2eccc9
Mark Clearwater <csmacnz@csmac.co.nz>
2024-12-11 19:52:20 +1300

    2024 day11 part2 refactor required.

--- 2024/csharp/day11/Program.cs
+++ 2024/csharp/day11/Program.cs
@@ -5,27 +5,24 @@ Console.WriteLine("Hello, World!");
 
 var input = File.ReadAllText("puzzle.txt").Split().Select(long.Parse).ToArray();
 
-foreach (var _ in Enumerable.Repeat(0, 25))
-{
-    var result = new List<long>();
-    foreach (var num in input)
-    {
-        if (num == 0)
-        {
-            result.Add(1);
-        }
-        else if ($"{num}".Length % 2 == 0)
-        {
-            var str = $"{num}";
-            result.Add(long.Parse(str[..(str.Length / 2)]));
-            result.Add(long.Parse(str[(str.Length / 2)..]));
-        }
-        else
-        {
-            result.Add(num * 2024);
-        }
-    }
-    input = [.. result];
-}
+var cache = new Dictionary<(long value, int repeats), long>();
 
-Console.WriteLine(input.Length);
+Console.WriteLine("Part1: " + input.Select(n => HowMany(n, 25)).Sum());
+
+Console.WriteLine("Part1: " + input.Select(n => HowMany(n, 75)).Sum());
+
+long HowMany(long value, int repeats)
+{
+    if (cache.TryGetValue((value, repeats), out var result)) return result;
+    result = (repeats, value) switch
+    {
+        (1, 0) => 1,
+        (1, var x) when ($"{x}".Length % 2 == 0) => 2,
+        (1, _) => 1,
+        (_, 0) => HowMany(1, repeats - 1),
+        (_, var x) when ($"{x}".Length % 2 == 0) => HowMany(long.Parse($"{x}"[..($"{x}".Length / 2)]), repeats - 1) + HowMany(long.Parse($"{x}"[($"{x}".Length / 2)..]), repeats - 1),
+        _ => HowMany(value * 2024, repeats - 1)
+    };
+    cache[(value, repeats)] = result;
+    return result;
+}
