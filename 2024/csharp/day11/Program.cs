42b04c9272ab63686916fb3b7df7cd0805be37c1
Mark Clearwater <csmacnz@csmac.co.nz>
2024-12-11 19:31:38 +1300

    2024 day11 part1

---
+++ 2024/csharp/day11/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO.Pipelines;
+
+Console.WriteLine("Hello, World!");
+
+var input = File.ReadAllText("puzzle.txt").Split().Select(long.Parse).ToArray();
+
+foreach (var _ in Enumerable.Repeat(0, 25))
+{
+    var result = new List<long>();
+    foreach (var num in input)
+    {
+        if (num == 0)
+        {
+            result.Add(1);
+        }
+        else if ($"{num}".Length % 2 == 0)
+        {
+            var str = $"{num}";
+            result.Add(long.Parse(str[..(str.Length / 2)]));
+            result.Add(long.Parse(str[(str.Length / 2)..]));
+        }
+        else
+        {
+            result.Add(num * 2024);
+        }
+    }
+    input = [.. result];
+}
+
+Console.WriteLine(input.Length);
