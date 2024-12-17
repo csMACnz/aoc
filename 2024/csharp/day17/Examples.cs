98f440a3753c7da91d72452687d92c68e713a5a2
Mark Clearwater <csmacnz@csmac.co.nz>
2024-12-18 12:21:31 +1300

    2024 day17 part2 passing

--- 2024/csharp/day17/Examples.cs
+++ 2024/csharp/day17/Examples.cs
@@ -32,7 +32,7 @@ public sealed class Examples
     {
         // If register A contains 10, the program 5,0,5,1,5,4 would output 0,1,2.
         var result = Processor.Run(10, 0, 0, [5, 0, 5, 1, 5, 4]);
-        CollectionAssert.AreEquivalent([0, 1, 2], result.output, EqualityComparer<int>.Default);
+        CollectionAssert.AreEquivalent<byte>([0, 1, 2], result.output, EqualityComparer<byte>.Default);
     }
 
     [TestMethod]
@@ -40,7 +40,7 @@ public sealed class Examples
     {
         // If register A contains 2024, the program 0,1,5,4,3,0 would output 4,2,5,6,7,7,7,7,3,1,0 and leave 0 in register A.
         var result = Processor.Run(2024, 0, 0, [0, 1, 5, 4, 3, 0]);
-        CollectionAssert.AreEquivalent([4, 2, 5, 6, 7, 7, 7, 7, 3, 1, 0], result.output, EqualityComparer<int>.Default);
+        CollectionAssert.AreEquivalent<byte>([4, 2, 5, 6, 7, 7, 7, 7, 3, 1, 0], result.output, EqualityComparer<byte>.Default);
         Assert.AreEqual(0, result.A);
     }
 
@@ -52,16 +52,51 @@ public sealed class Examples
         // Register C: 0
 
         // Program: 0,1,5,4,3,0
-        var result = Processor.Run(729, 0, 0, [0, 1, 5, 4, 3, 0]);
-        CollectionAssert.AreEquivalent([4, 6, 3, 5, 6, 3, 5, 2, 1, 0], result.output, EqualityComparer<int>.Default);
+        var (_, _, _, output) = Processor.Run(729, 0, 0, [0, 1, 5, 4, 3, 0]);
+        CollectionAssert.AreEquivalent<byte>([4, 6, 3, 5, 6, 3, 5, 2, 1, 0], output, EqualityComparer<byte>.Default);
     }
 
     [TestMethod]
-    public void PuzzleFromFile()
+    public void PuzzleFromFilePart1()
     {
         var input = File.ReadAllLines("puzzle.txt");
-        var result = Processor.Run(input);
-        Console.WriteLine("part1: " + string.Join(",", result.output));
-        CollectionAssert.AreEquivalent([1, 3, 5, 1, 7, 2, 5, 1, 6], result.output, EqualityComparer<int>.Default);
+        var (_, _, _, output) = Processor.Run(input);
+        Console.Error.WriteLine("part1: " + string.Join(",", output));
+        CollectionAssert.AreEquivalent<byte>([1, 3, 5, 1, 7, 2, 5, 1, 6], output, EqualityComparer<byte>.Default);
+    }
+
+    [TestMethod]
+    public void SamplePart2()
+    {
+        var (_, _, _, instructions) = Processor.Parse([
+            "Register A: 2024",
+            "Register B: 0",
+            "Register C: 0",
+            "",
+            "Program: 0,3,5,4,3,0"
+            ]);
+        var number = Processor.Part2(instructions, 0, 0);
+
+        Console.Error.WriteLine("part2: " + number);
+        Assert.AreEqual(117440, number);
+    }
+
+    [TestMethod]
+    public void PuzzleFromFilePart2()
+    {
+        // 2,4 B = A % 8
+        // 1,3 B = B ^ 3
+        // 7,5 C = A >> B
+        // 4,7 B = B ^ C
+        // 0,3 A = A >> 3
+        // 1,5 B = B ^ 5
+        // 5,5 Print(B%8)
+        // 3,0 = GOTO 0
+        var input = File.ReadAllLines("puzzle.txt");
+        var (_, _, _, instructions) = Processor.Parse(input);
+        var number = Processor.Part2(instructions, 0, 0);
+
+        Console.Error.WriteLine("part2: " + number);
+        Assert.AreEqual(236555997372013, number);
     }
 }
--- 2024/csharp/day17/Processor.cs
+++ 2024/csharp/day17/Processor.cs
@@ -2,20 +2,71 @@ using System.Diagnostics;
 
 public static class Processor
 {
-    internal static (int A, int B, int C, ICollection<int> output) Run(string[] sample)
+    public static (long A, long B, long C, ICollection<byte> output) Run(string[] sample)
+    {
+        var (A, B, C, instructions) = Parse(sample);
+        return Run(A, B, C, instructions);
+    }
+
+    public static (long A, long B, long C, List<byte> instructions) Parse(string[] sample)
     {
         var A = int.Parse(sample[0][12..]);
         var B = int.Parse(sample[1][12..]);
         var C = int.Parse(sample[2][12..]);
-        var instructions = sample[4][9..].Split(",").Select(int.Parse).ToList();
-        return Processor.Run(A, B, C, instructions);
+        var instructions = sample[4][9..].Split(",").Select(byte.Parse).ToList();
+        return (A, B, C, instructions);
     }
 
-    public static (int A, int B, int C, ICollection<int> output) Run(int A, int B, int C, List<int> instructions)
+    public static long? Part2(List<byte> instructions, int length, long input)
+    {
+        if (length == instructions.Count) return input;
+        var expectedOutput = new List<byte>(instructions)[(instructions.Count - 1 - length)..];
+        // Console.Error.WriteLine($"{length}/{instructions.Count}, {input:b}({input})");
+        // Console.Error.WriteLine($"I:{string.Join(",", instructions)}");
+        // Console.Error.WriteLine($"{string.Join(",", expectedOutput)}");
+        {
+            foreach (var n in Enumerable.Range(0, 1 << 3))
+            {
+                var test = (input << 3) | (long)n;
+                int indexOffset = 0;
+                int outputCount = 0;
+                var valid = true;
+                long A = test, B = 0, C = 0;
+                while (indexOffset < instructions.Count)
+                {
+                    (A, B, C, indexOffset, var output) = Step(A, B, C, instructions, indexOffset);
+                    if (output is not null)
+                    {
+                        if (outputCount >= expectedOutput.Count || expectedOutput[outputCount] != output)
+                        {
+                            valid = false;
+                            break;
+                        }
+                        outputCount++;
+                    }
+                }
+                
+                if (valid)
+                {
+                    Console.Error.WriteLine($"O:{string.Join(",", expectedOutput)}({test:b})");
+                    var match = Part2(instructions, length + 1, test);
+                    if (match.HasValue)
+                    {
+                        Console.WriteLine(match.Value);
+                        return match.Value;
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public static (long A, long B, long C, ICollection<byte> output) Run(long A, long B, long C, List<byte> instructions)
     {
         int indexOffset = 0;
-        int? output = null;
-        var outputList = new List<int>();
+        byte? output = null;
+        var outputList = new List<byte>();
         while (indexOffset < instructions.Count)
         {
             (A, B, C, indexOffset, output) = Step(A, B, C, instructions, indexOffset);
@@ -27,9 +78,9 @@ public static class Processor
         return (A, B, C, outputList);
     }
 
-    public static (int A, int B, int C, int nextIndex, int? output) Step(int A, int B, int C, List<int> instructions, int indexOffset)
+    public static (long A, long B, long C, int nextIndex, byte? output) Step(long A, long B, long C, List<byte> instructions, int indexOffset)
     {
-        int comboOperand(int operand)
+        long comboOperand(byte operand)
         {
             return operand switch
             {
@@ -47,26 +98,27 @@ public static class Processor
             };
         }
 
+        var operand = instructions[indexOffset + 1];
         return instructions[indexOffset] switch
         {
             // adv instruction (opcode 0) performs division
-            0 => ((int)(A / Math.Pow(2, comboOperand(instructions[indexOffset + 1]))), B, C, indexOffset + 2, null),
+            0 => (A >> (int)comboOperand(operand), B, C, indexOffset + 2, null),
             // bxl instruction (opcode 1) calculates the bitwise XOR of register B and the instruction's literal operand, then stores the result in register B.
-            1 => (A, B ^ instructions[indexOffset + 1], C, indexOffset + 2, null),
+            1 => (A, B ^ operand, C, indexOffset + 2, null),
             // bst instruction (opcode 2) calculates the value of its combo operand modulo 8 (thereby keeping only its lowest 3 bits), then writes that value to the B register.
-            2 => (A, comboOperand(instructions[indexOffset + 1]) % 8, C, indexOffset + 2, null),
+            2 => (A, comboOperand(operand) % 8, C, indexOffset + 2, null),
             // jnz instruction (opcode 3) does nothing if the A register is 0.
             3 when A is 0 => (A, B, C, indexOffset + 2, null),
             // However, if the A register is not zero, it jumps by setting the instruction pointer to the value of its literal operand; if this instruction jumps, the instruction pointer is not increased by 2 after this instruction.
-            3 => (A, B, C, instructions[indexOffset + 1], null),
+            3 => (A, B, C, operand, null),
             // The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C, then stores the result in register B.
             4 => (A, B ^ C, C, indexOffset + 2, null),
             // The out instruction (opcode 5) calculates the value of its combo operand modulo 8, then outputs that value.
-            5 => (A, B, C, indexOffset + 2, comboOperand(instructions[indexOffset + 1]) % 8),
+            5 => (A, B, C, indexOffset + 2, (byte)(comboOperand(operand) % 8)),
             // bdv instruction (opcode 6) works exactly like the adv instruction except that the result is stored in the B register.
-            6 => (A, (int)(A / Math.Pow(2, comboOperand(instructions[indexOffset + 1]))), C, indexOffset + 2, null),
+            6 => (A, A >> (int)comboOperand(operand), C, indexOffset + 2, null),
             // The cdv instruction (opcode 7) works exactly like the adv instruction except that the result is stored in the C register.
-            7 => (A, B, (int)(A / Math.Pow(2, comboOperand(instructions[indexOffset + 1]))), indexOffset + 2, null),
+            7 => (A, B, A >> (int)comboOperand(operand), indexOffset + 2, null),
             _ => throw new UnreachableException("Out of bounds value")
         };
 
