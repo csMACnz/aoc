604181acffa587847a20b4c8a493f873c07fa3aa
Mark Clearwater <mclearwater@gmail.com>
2024-12-22 09:02:53 +1300

    2024 day20 part1 refactor

--- 2024/csharp/day20/Program.cs
+++ 2024/csharp/day20/Program.cs
@@ -32,39 +32,67 @@ foreach (var (row, line) in lines.Index())
 Console.WriteLine(start);
 Console.WriteLine(end);
 
+var basePath = CalculateSteps(grid, start, end);
+var baseCount = basePath.Count - 1;
+Console.WriteLine("(9376) Part 0: " + baseCount);
 
-var baseCount = CalculateSteps(grid);
-Console.WriteLine("Part 0: " + baseCount);
+var cheatSize = 2;
 var result = 0;
-foreach (var row in Enumerable.Range(0, rowCount))
+foreach (var index in Enumerable.Range(0, baseCount))
 {
-    foreach (var col in Enumerable.Range(0, colCount))
+    var cheatStartPos = basePath[index];
+    foreach (var yx in OffsetsToCheck(cheatSize))
     {
-        Console.Write(".");
-        if (grid[row, col])
+        var cheatEndPos = (row: cheatStartPos.row + yx.row, col: cheatStartPos.col + yx.col);
+        if (cheatEndPos.row < 0 || cheatEndPos.row >= rowCount ||
+            cheatEndPos.col < 0 || cheatEndPos.col >= colCount) continue;
+        if (grid[cheatEndPos.row, cheatEndPos.col]) continue;
+        var postCheatSteps = CalculateSteps(grid, cheatEndPos, end);
+        var ans = index + cheatSize + postCheatSteps.Count - 1;
+        if (baseCount - ans >= 100)
         {
-            grid[row, col] = false;
-            var ans = CalculateSteps(grid);
-            if (baseCount - ans >= 100)
-            {
-                result++;
-            }
-            grid[row, col] = true;
+            result++;
         }
     }
-
-    Console.WriteLine();
 }
 
+Console.WriteLine("(1343) Part 1: " + result);
+return;
 
-Console.WriteLine("Part 1: " + result);
+// var result = Part1BruteForce(grid);
+// Console.WriteLine("(1343) Part 1: " + result);
 
-int CalculateSteps(bool[,] grid)
+int Part1BruteForce(bool[,] grid)
 {
     int rowCount = grid.GetLength(0);
     int colCount = grid.GetLength(1);
-    var queue = new Queue<((int row, int col) pos, int count)>();
-    queue.Enqueue((start, 0));
+    var result = 0;
+    foreach (var row in Enumerable.Range(0, rowCount))
+    {
+        foreach (var col in Enumerable.Range(0, colCount))
+        {
+            Console.Write(".");
+            if (grid[row, col])
+            {
+                grid[row, col] = false;
+                var ans = CalculateSteps(grid, start, end).Count;
+                if (baseCount - ans >= 100)
+                {
+                    result++;
+                }
+                grid[row, col] = true;
+            }
+        }
+    }
+    return result;
+}
+
+static List<(int row, int col)> CalculateSteps(bool[,] grid, (int, int) start, (int, int) end)
+{
+    int rowCount = grid.GetLength(0);
+    int colCount = grid.GetLength(1);
+    var queue = new Queue<Node>();
+    queue.Enqueue(new Node(null, start, 0));
     var seen = new bool[rowCount, colCount];
     while (queue.TryDequeue(out var item))
     {
@@ -73,13 +101,51 @@ int CalculateSteps(bool[,] grid)
         seen[item.pos.row, item.pos.col] = true;
         if (item.pos == end)
         {
-            return item.count;
+            var list = new List<(int row, int col)>
+            {
+                item.pos
+            };
+            while (item.prev is not null)
+            {
+                item = item.prev;
+                list.Add(item.pos);
+            }
+            list.Reverse();
+            return list;
         }
         if (grid[item.pos.row, item.pos.col]) continue;
-        queue.Enqueue(((item.pos.row + 1, item.pos.col), item.count + 1));
-        queue.Enqueue(((item.pos.row - 1, item.pos.col), item.count + 1));
-        queue.Enqueue(((item.pos.row, item.pos.col + 1), item.count + 1));
-        queue.Enqueue(((item.pos.row, item.pos.col - 1), item.count + 1));
+        queue.Enqueue(new Node(item, (item.pos.row + 1, item.pos.col), item.count + 1));
+        queue.Enqueue(new Node(item, (item.pos.row - 1, item.pos.col), item.count + 1));
+        queue.Enqueue(new Node(item, (item.pos.row, item.pos.col + 1), item.count + 1));
+        queue.Enqueue(new Node(item, (item.pos.row, item.pos.col - 1), item.count + 1));
     }
-    return int.MinValue;
+    return null!;
 }
+
+IEnumerable<(int row, int col)> OffsetsToCheck(int range)
+{
+    foreach (var i in Enumerable.Range(0, range + 1))
+    {
+        foreach (var j in Enumerable.Range(0, range + 1 - i))
+        {
+            if (i != 0 || j != 0)
+            {
+                yield return (i, j);
+                if (i != 0)
+                {
+                    yield return (-i, j);
+                }
+                if (j != 0)
+                {
+                    yield return (i, -j);
+                    if (i != 0)
+                    {
+                        yield return (-i, -j);
+                    }
+                }
+            }
+        }
+    }
+}
+
+record class Node(Node? prev, (int row, int col) pos, int count);
