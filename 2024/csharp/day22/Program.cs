f61656f78d9f2787ac59af3645985692b0cc15e5
Mark Clearwater <mclearwater@gmail.com>
2024-12-23 22:36:05 +1300

    2024 day 22 part 2

--- 2024/csharp/day22/Program.cs
+++ 2024/csharp/day22/Program.cs
@@ -1,21 +1,79 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Diagnostics;
+
 Console.WriteLine("Hello, World!");
 
 var inputs = File.ReadAllLines("puzzle.txt").Select(long.Parse).ToArray();
 
-var result = 0L;
-foreach (var input in inputs)
-{
-    var end = puzzle(input, 2000);
-    Console.WriteLine($"{input}: {end}");
-    checked
-    {
-        result += end;
-    }
-}
-Console.WriteLine("Part1: " + result);
+var part1Result = Part1(inputs);
+Console.WriteLine("Part1: " + part1Result);
 
-long puzzle(long seed, int iterations)
+var part2Result = Part2(inputs);
+Console.WriteLine("Part2: " + part2Result);
+
+static long Part2(long[] inputs)
+{
+    var options = GetPossibilities(inputs);
+    var bestScore = options.Max(static o => o.Value.Values.Sum(static v => v));
+    return bestScore;
+}
+
+static Dictionary<(short a, short b, short c, short d), Dictionary<int, short>> GetPossibilities(long[] inputs)
+{
+    Dictionary<(short a, short b, short c, short d), Dictionary<int, short>> result = [];
+    foreach (var (index, input) in inputs.Index())
+    {
+        var diffs = new List<short>();
+        var prev = input;
+        foreach (var iteration in Enumerable.Range(0, 2000))
+        {
+            var end = Psuedo(prev);
+            short endPrice = (short)(end % 10);
+            short diff = (short)(endPrice - (prev % 10));
+            diffs.Add(diff);
+            if (diffs.Count == 5)
+            {
+                diffs.RemoveAt(0);
+            }
+            if (diffs.Count > 4) throw new UnreachableException("bad option");
+            if (diffs.Count == 4)
+            {
+                var key = (diffs[0], diffs[1], diffs[2], diffs[3]);
+                if (result.TryGetValue(key, out var dict))
+                {
+                    if (!dict.ContainsKey(index))
+                    {
+                        dict[index] = endPrice;
+                    }
+                }
+                else
+                {
+                    result[key] = new Dictionary<int, short> { { index, endPrice } };
+                }
+            }
+            prev = end;
+        }
+    }
+    return result;
+}
+
+static long Part1(long[] inputs)
+{
+    var result = 0L;
+    foreach (var input in inputs)
+    {
+        var end = puzzle(input, 2000);
+        // Console.WriteLine($"{input}: {end}");
+        checked
+        {
+            result += end;
+        }
+    }
+    return result;
+}
+
+static long puzzle(long seed, int iterations)
 {
     long result = seed;
     foreach (var iteration in Enumerable.Range(0, iterations))
@@ -24,7 +82,8 @@ long puzzle(long seed, int iterations)
     }
     return result;
 }
-long Psuedo(long seed)
+
+static long Psuedo(long seed)
 {
     var shift = seed << 6; // * 64 == * 2^6 == << 6
     var result = (seed ^ shift) % 16777216;
