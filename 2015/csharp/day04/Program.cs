0216a78a10d1ada473ec5779eaaa0b1fecab4dd8
Mark Clearwater <mclearwater@gmail.com>
2024-12-27 14:43:15 +1300

    2015 day04

---
+++ 2015/csharp/day04/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+var input = File.ReadAllText("puzzle.txt");
+
+var i = 1;
+while (true)
+{
+    var hash = MD5.HashData(Encoding.ASCII.GetBytes(input + i));
+    var test = Convert.ToHexString(hash).Replace("-", "");
+    if (test.StartsWith("00000")) { break; }
+    checked
+    {
+        i++;
+    }
+}
+Console.WriteLine($"Part1: " + i);
+
+var j = 1;
+while (true)
+{
+    var hash = MD5.HashData(Encoding.ASCII.GetBytes(input + j));
+    var test = Convert.ToHexString(hash).Replace("-", "");
+    if (test.StartsWith("000000")) { break; }
+    checked
+    {
+        j++;
+    }
+}
+Console.WriteLine($"Part2: " + j);
