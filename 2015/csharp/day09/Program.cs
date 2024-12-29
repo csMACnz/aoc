9a69258c3a67f7e7b7e11fe8d9544bc336fba682
Mark Clearwater <mclearwater@gmail.com>
2024-12-29 15:44:22 +1300

    2015 day09 part1

---
+++ 2015/csharp/day09/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var distances = File.ReadLines("puzzle.txt")
+.SelectMany<string, Edge>(l =>
+{
+    //AlphaCentauri to Snowdin = 66
+    var parts = l.Split(" ");
+    var from = parts[0];
+    var to = parts[2];
+    var distance = long.Parse(parts[4]);
+    return [new Edge(from, to, distance), new Edge(to, from, distance)];
+}).ToArray();
+
+var destinations = distances.SelectMany<Edge, string>(d => [d.From, d.To]).Distinct().ToArray();
+var queue = new PriorityQueue<(string current, string[] remainder), long>();
+foreach (var destination in destinations)
+{
+    queue.Enqueue((destination, destinations.Where(d => d != destination).ToArray()), 0L);
+}
+
+while (queue.TryDequeue(out var item, out long score))
+{
+    if (item.remainder.Length == 0)
+    {
+        Console.WriteLine("Part1: " + score);
+        break;
+    }
+    foreach (var next in item.remainder.Select(d => distances.FirstOrDefault(e => e.From == item.current && e.To == d)))
+    {
+        if (next is not null)
+        {
+            queue.Enqueue((next.To, item.remainder.Where(d => d != next.To).ToArray()), score + next.Distance);
+        }
+    }
+}
+record class Edge(string From, string To, long Distance);
