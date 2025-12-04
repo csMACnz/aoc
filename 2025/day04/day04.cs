5dd97b44f13529ef283d7aaacf91de33bfe08bbb
Mark Clearwater <csmacnz@csmac.co.nz>
2025-12-05 10:16:11 +1300

    2025 day04 slight refactor

--- 2025/day04/day04.cs
+++ 2025/day04/day04.cs
@@ -19,7 +19,7 @@ var testInput = """
     @.@.@@@.@.
     """;
 
-Console.WriteLine($"Expected: (13, 0)");
+Console.WriteLine($"Expected: (13, 43)");
 Console.WriteLine($" Example: {Puzzle(testInput)}");
 
 HttpClient client = new HttpClient();
@@ -34,11 +34,10 @@ Console.WriteLine($"   Day04: {Puzzle(content)}");
 
 static (long one, long two) Puzzle(ReadOnlySpan<char> input)
 {
-    var lines = input.EnumerateLines();
     var rowOffset = 0;
     var colCount = 0;
-    Dictionary<(int, int), int> rollPositions = [];
-    foreach (var line in lines)
+    HashSet<(int, int)> rollPositions = [];
+    foreach (var line in input.EnumerateLines())
     {
         colCount = line.Length;
         var colOffset = 0;
@@ -47,7 +46,7 @@ static (long one, long two) Puzzle(ReadOnlySpan<char> input)
             if (c == '@')
             {
                 // Process '@' character
-                rollPositions[(rowOffset, colOffset)] = 1;
+                rollPositions.Add((rowOffset, colOffset));
             }
             colOffset++;
         }
@@ -70,21 +69,23 @@ static (long one, long two) Puzzle(ReadOnlySpan<char> input)
     return (part1, part2);
 }
 
-static List<(int row, int col)> GetRemovableRolls(Dictionary<(int, int), int> rollPositions, int rowCount, int colCount)
+static List<(int row, int col)> GetRemovableRolls(HashSet<(int, int)> rollPositions, int rowCount, int colCount)
 {
+    (int, int)[] offsets = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)];
+            
     var removeList = new List<(int, int)>();
     for (var row = 0; row < rowCount; row++)
     {
         for (var col = 0; col < colCount; col++)
         {
-            if (rollPositions.ContainsKey((row, col)))
+            if (rollPositions.Contains((row, col)))
             {
                 var neighborCount = 0;
-                foreach ((int, int) offset in new[] { (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1) })
+                foreach ((int rowOffset, int colOffset) offset in offsets)
                 {
-                    var nRow = row + offset.Item1;
-                    var nCol = col + offset.Item2;
-                    if (rollPositions.ContainsKey((nRow, nCol)))
+                    var nRow = row + offset.rowOffset;
+                    var nCol = col + offset.colOffset;
+                    if (rollPositions.Contains((nRow, nCol)))
                     {
                         neighborCount++;
                     }
