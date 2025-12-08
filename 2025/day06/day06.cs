e98345ed8def0f3e1580351b60314950fe198557
Mark Clearwater <csmacnz@csmac.co.nz>
2025-12-09 09:47:46 +1300

    2025 day06 part2

--- 2025/day06/day06.cs
+++ 2025/day06/day06.cs
@@ -1,10 +1,6 @@
 #!/usr/local/share/dotnet/dotnet run
 #:package Colorful.Console@1.2.15
 
-using System.Data;
-using System.Runtime.CompilerServices;
-using System.Text;
-
 Colorful.Console.WriteAscii("AoC 2025 Day 6");
 
 var testInput = """
@@ -14,7 +10,7 @@ var testInput = """
     *   +   *   +  
     """;
 
-Console.WriteLine($"Expected: (4277556, 0)");
+Console.WriteLine($"Expected: (4277556, 3263827)");
 Console.WriteLine($" Example: {Puzzle(testInput)}");
 
 HttpClient client = new HttpClient();
@@ -89,5 +85,88 @@ static (long one, long two) Puzzle(ReadOnlySpan<char> input)
         part1 += result;
     }
 
-    return (part1, 0L);
+    var lines = input.EnumerateLines();
+    lines.MoveNext();
+    var numbers = new long?[lines.Current.Length];
+    for (var i = 0; i < numbers.Length; i++)
+    {
+        if (lines.Current[i] == ' ')
+        {
+            numbers[i] = null;
+            continue;
+        }
+        numbers[i] = lines.Current[i] - '0';
+    }
+    while (lines.MoveNext())
+    {
+        if (lines.Current[0] == '*' || lines.Current[0] == '+')
+        {
+            break;
+        }
+        for (var i = 0; i < numbers.Length; i++)
+        {
+            if (lines.Current[i] != ' ')
+            {
+                long digit = lines.Current[i] - '0';
+                numbers[i] = (numbers[i] ?? 0L) * 10L + digit;
+            }
+        }
+    }
+    var actions = new List<(char op, int index, int length)>();
+    char lastSymbol = lines.Current[0];
+    int lastIndex = 0;
+    int length = 0;
+    for (var i = 1; i < lines.Current.Length; i++)
+    {
+        if (lines.Current[i] == '*' || lines.Current[i] == '+')
+        {
+            actions.Add((lastSymbol, lastIndex, length));
+            lastSymbol = lines.Current[i];
+            lastIndex = i;
+            length = 0;
+        }
+        else
+        {
+            length++;
+        }
+    }
+    actions.Add((lastSymbol, lastIndex, numbers.Length - lastIndex));
+    var part2 = 0L;
+    checked
+    {
+        foreach (var action in actions)
+        {
+            var op = action.op;
+            var index = action.index;
+            var len = action.length;
+            long result = 0;
+            // Console.WriteLine($"OPValue at {index} is {op} for length {len}");
+            for (var i = index; i < index + len; i++)
+            {
+                long? v = numbers[i];
+                if (v.HasValue)
+                {
+                    // Console.WriteLine($"Value at {i} is {v.Value}");
+                    if (result == 0)
+                    {
+                        result = v.Value;
+                    }
+                    else
+                    {
+                        if (op == '*')
+                        {
+                            result *= v.Value;
+                        }
+                        else if (op == '+')
+                        {
+                            result += v.Value;
+                        }
+                    }
+                }
+            }
+            part2 += result;
+        }
+    }
+
+    return (part1, part2);
 }
